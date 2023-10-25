d082c91f39e65268cd66fef6cb569e284f13e058
Drew Atkison <dtatkison@gmail.com>
2023-10-24 22:51:04 -0500

    another fix

--- apps/family-budget-api/src/main.ts
+++ apps/family-budget-api/src/main.ts
@@ -14,10 +14,7 @@ async function bootstrap() {
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
 
-  // app.enableCors({
-  //   origin: "*",
-  //   credentials: true
-  // });
+  app.enableCors();
 
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
