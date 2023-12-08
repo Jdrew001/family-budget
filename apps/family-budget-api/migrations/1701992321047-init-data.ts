073f70de84e96b781792ed3a6c4a04ea69e2b54f
Drew Atkison <dtatkison@gmail.com>
2023-12-07 21:34:41 -0600

    fixed migrations

--- apps/family-budget-api/migrations/1701992321047-init-data.ts
+++ apps/family-budget-api/migrations/1701992321047-init-data.ts
@@ -7,29 +7,22 @@ export class InitData1701992321047 implements MigrationInterface {
         await queryRunner.query(`INSERT INTO account_type (name) VALUES ('Savings')`);
         await queryRunner.query(`INSERT INTO account_type (name) VALUES ('Credit Card')`);
 
+        await queryRunner.query(`INSERT INTO subscription (name, description, price) VALUES ('Free', 'Free Subscription', 0);`);
+
         await queryRunner.query(`
-
-            INSERT INTO subscription (name, description, price) VALUES 
-                ('Free', 'Free Subscription, 0);
-
             INSERT INTO budget_period (frequency) VALUES (0);
             INSERT INTO budget_period (frequency) VALUES (1);
             INSERT INTO budget_period (frequency) VALUES (2);
             INSERT INTO budget_period (frequency) VALUES (3);
             INSERT INTO budget_period (frequency) VALUES (4);
-
             INSERT INTO frequency_ref (id, name, type) VALUES
             (1, 'Weekly', 0);
-        
             INSERT INTO frequency_ref (id, name, type) VALUES
                 (2, 'Bi-Weekly', 1);
-                
             INSERT INTO frequency_ref (id, name, type) VALUES
                 (3, 'Monthly', 2);
-
             INSERT INTO frequency_ref (id, name, type) VALUES
                 (4, 'Quarterly', 3);
-
             INSERT INTO frequency_ref (id, name, type) VALUES
                 (5, 'Annually', 4);
         `);
