96ac4fa6d55469c564cfee6ba52118f549c899de
Drew Atkison <dtatkison@gmail.com>
2023-12-08 08:33:30 -0600

    fixed

--- libs/family-budget.model/src/lib/entities/frequency-ref.model.ts
+++ libs/family-budget.model/src/lib/entities/frequency-ref.model.ts
@@ -1,10 +1,10 @@
-import { Column, Entity, Int32, PrimaryColumn } from "typeorm";
+import { Column, Entity, Int32, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
 import { Frequency } from "@family-budget/family-budget.model";
 
 @Entity('frequency_ref')
 export class FrequencyRef {
 
-    @PrimaryColumn()
+    @PrimaryGeneratedColumn({type: "int"})
     id: number;
 
     @Column()
